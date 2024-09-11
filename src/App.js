6bf89eb62d2a9d6113a198442e0d39eb76ba4d76
gulggum <163222604+gulggum@users.noreply.github.com>
2024-09-11 20:30:29 +0900

    finish
    로컬스토리지 재검토해야함

--- src/App.js
+++ src/App.js
@@ -1,5 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaTrashCan } from "react-icons/fa6";
+import styles from "./App.module.css";
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
-function App() {}
+  useEffect(() => {
+    const saveTodos = JSON.parse(localStorage.getItem("todo")) || [];
+    setTodoList(saveTodos);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  }, [todoList]); //todoList가 바뀔때 마다 실행
+
+  const onInput = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setTodo("");
+    if (todo === "") {
+      return;
+    }
+    setTodoList((currentText) => [todo, ...currentText]);
+  };
+
+  const onDelete = (index) => {
+    setTodoList(todoList.filter((_, todoIndex) => index !== todoIndex));
+  };
+  return (
+    <div className={styles.container}>
+      <h1>Todo List</h1>
+      <div className={styles.todoListBox}>
+        <ul>
+          {todoList.map((text, index) => (
+            <li key={index}>
+              <span>{text}</span>
+              <button
+                className={styles.deleteBtn}
+                onClick={() => onDelete(index)}
+              >
+                <FaTrashCan />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <form onSubmit={onSubmit}>
+        <input
+          onInput={onInput}
+          value={todo}
+          type="text"
+          placeholder="텍스트를 작성해 주세요..😍"
+        ></input>
+        <button className={styles.submitBtn} type="submit">
+          입력
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default App;
